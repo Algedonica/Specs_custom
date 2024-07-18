84db064a70868b370565b4c0fb9649b1b6b8a3b0
Algedonica <83134187+Algedonica@users.noreply.github.com>
2024-07-18 13:57:26 +0300

    upd

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CustomWebRTC",
-            url: "https://github.com/Algedonica/Specs_custom/blob/main/CustomWebRTC.xcframework.zip",
-            checksum: "3760f129d10ed8332a4e4a6f09600222848f548a5948a097bd2130e22108a188"
+            url: "https://github.com/Algedonica/Specs_custom/blob/main/CustomWebRTC.xcframework.zip"
         ),
     ]
 )
